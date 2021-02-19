6bde0185c614da11f2c677adde80665f909d88bf
Khaled Mohamed <khaledmzakria1@gmail.com>
2021-02-19 08:18:16 +0200

    actions and reducers init

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello, How are you?
+            </div>
+        )
+    }
+}
+
+export default App
